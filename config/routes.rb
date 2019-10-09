d238103eda4cbe612a6bb7d04a746560f61e92ef
Seika962 <56057625+Seika962@users.noreply.github.com>
2019-10-09 10:16:35 +0800

    Log Out (#7)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/signup', to: 'users#new'
   get '/login', to:'sessions#new'
+  delete '/logout', to:'sessions#destroy'
   
   resources :users
-  resources :sessions, only: :create
+  resources :sessions, only: [:create, :destroy]
 end
