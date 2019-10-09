a9badc291f5a6676f8ddfe08041a9238a8ed0973
Seika962 <56057625+Seika962@users.noreply.github.com>
2019-10-09 09:02:57 +0800

    [Function] Log In (#6)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+    @user=User.find_by(email:params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+      log_in(@user)
+      flash[:success]="successfully logged in."
+
+      redirect_to root_url
+    else
+      flash.now[:danger]="Invalid Credentials."
+      render 'new'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/login', to:'sessions#new'
   
   resources :users
+  resources :sessions, only: :create
 end
