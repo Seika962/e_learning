d09715bb2872e47f1e4b373b7ffcfdf012bff878
Seika962 <Seika0622@gmail.com>
2019-10-11 12:06:01 +0800

    Admin Function

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,14 @@ class Admin::UsersController < ApplicationController
     def index
         @users=User.paginate(page: params[:page], per_page: 10)
     end
+    def update
+        @user=User.find(params[:id])
+        @user.update_attribute(:is_admin, true)
+        redirect_to admin_users_path
+    end
+    def destroy
+        @user=User.find(params[:id])
+        @user.update_attribute(:is_admin, false)
+        redirect_to admin_users_path
+    end
 end
