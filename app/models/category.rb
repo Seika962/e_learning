97a84596efd770c698d9c32e297189c6f1165ee7
Seika962 <56057625+Seika962@users.noreply.github.com>
2019-10-14 12:44:39 +0800

    [layout] new word (#21)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
-    validates :title, presence: true, length: {maximum: 50 }
-    validates :description, presence: true, length: { maximum: 255 }
+  has_many :words
+  validates :title, presence: true, length: {maximum: 50 }
+  validates :description, presence: true, length: { maximum: 255 }
 end
