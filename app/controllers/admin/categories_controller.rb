a02142ba0d6604740d599354d782a06e43b12c2a
Seika962 <Seika0622@gmail.com>
2019-10-14 10:45:28 +0800

    [function] Category Index

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories=Category.all
   end
   def new
     @category= Category.new
@@ -11,6 +12,13 @@ class Admin::CategoriesController < ApplicationController
     else
       render 'new'
     end
+  end
+  def edit
+  end
+  def destroy
+    @category=Category.find(params[:id])
+    @category.destroy
+    redirect_to admin_categories_path
   end
 
   private
