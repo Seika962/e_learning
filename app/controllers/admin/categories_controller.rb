2daeb1fbbf1766ea781a67ec175a932534a34d60
Seika962 <56057625+Seika962@users.noreply.github.com>
2019-10-14 10:12:35 +0800

    [function] new category (#17)

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,4 +4,17 @@ class Admin::CategoriesController < ApplicationController
   def new
     @category= Category.new
   end
+  def create
+    @category=Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
 end
