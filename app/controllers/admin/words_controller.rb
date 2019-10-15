3f9833790c93af955e0587267ecb8e03de2e6fb4
Seika962 <Seika0622@gmail.com>
2019-10-15 10:07:31 +0800

    [function] new word

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -2,5 +2,20 @@ class Admin::WordsController < ApplicationController
   def new
     @category = Category.find(params[:category_id])
     @word = @category.words.new
+    3.times{@word.choices.new}
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @word = @category.words.new(word_params)
+    if @word.save
+      redirect_to admin_category_url(@category)
+    else
+      render 'new'
+    end
+  end
+
+  def word_params
+    params.require(:word).permit(:content, choices_attributes:[:id, :content, :is_correct])
   end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,17 @@
 class Word < ApplicationRecord
   belongs_to :category
+  has_many :choices, dependent: :destroy
+  accepts_nested_attributes_for :choices
+
+  validates :content, presence: true, length: {maximum: 50}
+  validate :has_one_correct
+
+  private
+  def has_one_correct
+    unless choices.collect {|choice| choice.is_correct? }.count(true) == 1
+      # collect the true choice checking one by one and count the number and check if there is only one of it
+      # just taking information from the form, not saving in the database
+      errors.add(:choices, "should have one correct answer")
+    end
+  end
 end
