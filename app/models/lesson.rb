2743796b5f0c186bd415363e7df96179b024878c
Seika962 <Seika0622@gmail.com>
2019-10-17 10:08:21 +0800

    added dependent::destroy

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,7 @@
 class Lesson < ApplicationRecord
     belongs_to :user
     belongs_to :category
-    has_many :answers
+    has_many :answers, dependent: :destroy
     has_many :words, through: :answers
 
     def next_word
