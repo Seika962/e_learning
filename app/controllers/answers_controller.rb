f136a3bfe7ac6ecd0f77b4c862e126e5582bbe1d
Seika962 <Seika0622@gmail.com>
2019-10-17 12:36:30 +0800

    Lesson Result Page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,17 +2,24 @@ class AnswersController < ApplicationController
   def new
     @lesson = Lesson.find(params[:lesson_id])
     @category = @lesson.category
-    @answer = @lesson.answers.new
+
+    if @lesson.next_word.nil?
+      @lesson.update_attribute(:result, @lesson.correct_answer)
+      redirect_to lesson_url(@lesson)
+      # show result
+    else
+      @answer = @lesson.answers.new
+    end
   end
   def create
     @lesson = Lesson.find(params[:lesson_id])
     @answer = @lesson.answers.new(answer_params)
-    # if @lesson.next_word.nil?
-    #   show result
-    # else
-      @answer.save
+    
+    if @answer.save
       redirect_to new_lesson_answer_url(@lesson)
-    # end
+    else
+      render 'new'
+    end
   end
 
   private
