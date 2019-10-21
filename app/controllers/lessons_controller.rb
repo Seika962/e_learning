5920aeaf749c9b372da3d8bea8e592fe4c03ff75
Seika962 <56057625+Seika962@users.noreply.github.com>
2019-10-21 08:52:16 +0800

    Lesson Result Page (#28)

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,12 @@
 class LessonsController < ApplicationController
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
   def create
-      @category = Category.find(params[:category_id])
-      @lesson = @category.lessons.new(user:current_user)
-        if @lesson.save
-          redirect_to new_lesson_answer_url(@lesson)
-        end
+    @category = Category.find(params[:category_id])
+    @lesson = @category.lessons.new(user:current_user)
+      if @lesson.save
+        redirect_to new_lesson_answer_url(@lesson)
+      end
   end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,12 +1,16 @@
 class Lesson < ApplicationRecord
-    belongs_to :user
-    belongs_to :category
-    has_many :answers, dependent: :destroy
-    has_many :words, through: :answers
+  belongs_to :user
+  belongs_to :category
+  has_many :answers, dependent: :destroy
+  has_many :words, through: :answers
+  has_many :choices, through: :answers
 
-    def next_word
-        (category.words - words).first
-        # first word of (all the words related to category - words related to answers which is the words you already answered)
-    end
-
+  def next_word
+    (category.words - words).first
+    # first word of (all the words related to category - words related to answers which is the words you already answered)
+  end
+  def correct_answer
+    choices.where(is_correct:true).count
+    # counting the number of correct answer in all the choices of the user
+  end
 end
