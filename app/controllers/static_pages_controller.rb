b20352229fdcccb843901372e50b51401d2323ca
Seika962 <Seika0622@gmail.com>
2019-10-23 12:35:47 +0800

    [Layout] Display Activities

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
+      @activities = Activity.where("user_id = ? OR user_id IN (?)", current_user.id, current_user.following.ids)
+    # when you want to get one user by one id, you can say "user_id = ?", but when there are many user you want to get, you have to say "user_id IN (?)"
       render "users/dash_board"
     end
   end
