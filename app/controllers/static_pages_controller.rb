7bfc8cbac091cbd694900e2a9cfb1028fbf77513
Seika962 <56057625+Seika962@users.noreply.github.com>
2019-10-24 09:30:33 +0800

    [Layout] Display Activities (#33)
    * [Layout] Display Activities
    
    * added path
    
    * added pagination
    
    * added link

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
+      @static_pages = Activity.where("user_id = ? OR user_id IN (?)", current_user.id, current_user.following.ids).paginate(page:params[:page],per_page:8)
+    # when you want to get one user by one id, you can say "user_id = ?", but when there are many user you want to get, you have to say "user_id IN (?)"
       render "users/dash_board"
     end
   end
