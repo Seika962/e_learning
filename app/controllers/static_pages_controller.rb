0ae7638641f447294a6b6d7f5530c61cbabdc0d6
Seika962 <Seika0622@gmail.com>
2019-10-25 12:16:27 +0800

    fixed link

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
+      
       @static_pages = Activity.where("user_id = ? OR user_id IN (?)", current_user.id, current_user.following.ids).order(created_at: :desc).paginate(page:params[:page],per_page:8)
     # when you want to get one user by one id, you can say "user_id = ?", but when there are many user you want to get, you have to say "user_id IN (?)"
+      @answers = current_user.answers
       render "users/dash_board"
     end
   end
